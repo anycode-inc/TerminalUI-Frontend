771bbbd5eb9d499e7f82328824da41487bb7a6c7
cankush625 <cankush625@gmail.com>
2021-03-18 01:02:44 +0530

    Application routes restructured
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- app.py
+++ app.py
@@ -9,13 +9,19 @@ def index():
 
 @app.route("/menu")
 def menu():
+    template = "menu.html"
+    return render_template(template)
+
+@app.route("/task")
+def task():
     choice = request.args.get("choice")
-    template = "index.html"
+    template = "menu.html"
     if choice == "1":
         url = "http://13.235.69.120/cgi-bin/TerminalUI-CGI/docker.py?x=python"
         res = req.get(url)
         contents = res.text
         template = "docker.html"
     else:
-        template = "index.html"
+        template = "menu.html"
     return render_template(template, response=contents)
+
